c7b8adb3edb00ce1906490c7906c81dd2dd93db0
Alex Romo <ayromo08@gmail.com>
2020-02-17 04:35:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,8 +68,10 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      $winner = "X"
       return true
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      $winner = "O"
       return true
     end
   end
