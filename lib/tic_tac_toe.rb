20f3a3f535167f946cef71d9f0f12a920373561f
Alex Romo <ayromo08@gmail.com>
2020-02-17 04:08:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,11 +54,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    "X"
-  else
-    "O"
-  end
+  turn_count(board).even? ? "X" : "O"
 end
 
 def won?(board)
