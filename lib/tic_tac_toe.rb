1f3ff7f30137d64fcf996fb8dd15614f65438aed
Alex Romo <ayromo08@gmail.com>
2020-02-17 03:59:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,9 +71,9 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if win_index_1 == "X" && win_index_2 == "X" && win_index_3 == "X"
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
       true
-    elsif win_index_1 == "O" && win_index_2 == "O" && win_index_3 == "O"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       true
     end
   end
