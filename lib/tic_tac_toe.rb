2fa7ab892293033c7e88f9fcc31fbc7c46354593
Alex Romo <ayromo08@gmail.com>
2020-02-17 03:26:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,9 +30,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index)
-    false
-  else
-    true
-  end
-end
+  index.between?(0,8) && position_taken?(board, index)
+END
